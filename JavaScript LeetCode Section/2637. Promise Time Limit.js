e96d94f3df5c569009482b290ad7b3ab1333d427
Jakub Szpytka <jakub.szpytka@gmail.com>
2023-04-15 14:39:26 +0200

    2637 promise time limit

---
+++ JavaScript LeetCode Section/2637. Promise Time Limit.js
@@ -0,0 +1,13 @@
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function(fn, t) {
+    return async function(...args) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => reject("Time Limit Exceeded"), t)
+            fn(...args).then(resolve, reject)
+        })
+    }
+};
