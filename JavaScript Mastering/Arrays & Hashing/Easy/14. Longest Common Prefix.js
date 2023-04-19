94b826a9e5b734a16d0a0da4fea8ea967018cd54
Jakub Szpytka <jakub.szpytka@gmail.com>
2023-04-19 11:44:03 +0200

    14 longest common prefix

---
+++ JavaScript Mastering/Arrays & Hashing/Easy/14. Longest Common Prefix.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+  if (strs.length === 0) return '';
+
+  let array = [];
+  let temp = '';
+  for (let i = 0; i < strs.length; i++) {
+    for (let j = 0; j < strs[0].length; j++) {
+      if (strs[0][j] === strs[i][j] && strs[i][j] !== undefined) {
+        temp += strs[0][j];
+      } else if (strs[0][j] !== strs[i][j]) {
+        break;
+      }
+    }
+    array.push(temp.length);
+    temp = '';
+  }
+
+  let resultLoop = array.sort((a, b) => a - b)[0];
+  if (resultLoop === 0) return '';
+  let result = '';
+  for (let i = 0; i < resultLoop; i++) {
+    result += strs[0][i];
+  }
+
+  return result;
+};
+
+console.log(longestCommonPrefix(['flower', 'flow', 'flight']));
+console.log(longestCommonPrefix(['dog', 'racecar', 'car']));
+console.log(longestCommonPrefix(['cir', 'car']));
